6ca7f9858412dd0d16375dd5532b2843bd748396
Sedat Dogan <sedatdoganm@gmail.com>
2025-09-03 12:33:21 +0300

    Arrays: declaration, instantiation and initialization

---
+++ Arrays/src/ArrayOperations.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class ArrayOperations {
+    public static void main(String[] args) {
+        /*
+        // ************Create an array*************
+
+        int[] arr = new int[]{1,2,3};
+        System.out.println(Arrays.toString(arr));
+
+        // Su sekilde de bir kontrol de yapabiliriz.
+
+        int []arr2 = new int[5];
+        arr2[0] = 5;
+        arr2[1] = 8;
+        System.out.println(Arrays.toString(arr2));
+        // output: [5, 8, 0, 0, 0] // Buradan da gordugumuz gibi int bir dizi'deki manuel degerler 0'dir.
+        */
+        
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+The files in this repository contain the files and code I created during Elshad Karimov's Data Structures course on Udemy.
